f4e9669d7b5b60ba5a95ffd012df09b7db7b8211
Lucas Felinto Pinto <lucasfepinto@gmail.com>
2020-04-06 09:01:07 -0300

    Update desafio1-1.js

--- Desafios/desafio1-1.js
+++ Desafios/desafio1-1.js
@@ -32,3 +32,16 @@ if (sexo == 'M') {
     console.log(`${nome1}, você ainda não pode se aposentar!`)
 }
 }
+// OUTRA FORMA DE RESOLVER USANDO MENOS LINHAS E IF/ELSE
+
+// const calculoContribuicao = idade + contribuicao
+
+// essas variáveis irão retornar true ou false
+// const homemPodeAposentar = sexo == 'M' && contribuicao >= 35 && calculoContribuicao >= 95
+// const mulherPodeAposentar = sexo == 'F' && contribuicao >= 30 && calculoContribuicao >= 85
+
+// if ( homemPodeAposentar || mulherPodeAposentar) {
+//     console.log(`${nome}, você pode se aposentar!`)
+// } else {
+//     console.log(`${nome}, você não pode se aposentar!`)
+// }
