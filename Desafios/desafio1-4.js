4203b0db2b7ff448b050dd29a66675350a18e81a
Lucas Felinto Pinto <lucasfepinto@gmail.com>
2020-04-06 15:47:17 -0300

    Update desafio1-4.js

--- Desafios/desafio1-4.js
+++ Desafios/desafio1-4.js
@@ -4,6 +4,7 @@ const user = {
     balance: 0
 };
 
+//Criar transação
 function createTransaction(transaction) {
     user.transactions.push(transaction) //Adiciona um elemento ao final de uma array e retorna o novo comprimento
 
@@ -14,3 +15,17 @@ function createTransaction(transaction) {
     }
 }
 
+//Relatórios
+function getHigherTransactionByType(type) {
+    let higherTransaction
+    let higherValue
+    
+    for (let transactions of user.transactions) {
+        if (transaction.type == type && trans.value > higherValue) {
+            higherTransaction = transaction.value
+            higherTransaction = value
+        }
+    }
+}
+
+return higherTransaction 
