d116c27e107a74759db94db652f47037025e8a21
Lucas Felinto Pinto <lucasfepinto@gmail.com>
2020-04-13 17:12:50 -0300

    modulo_03
    nonjucks configuret
    server configuret
    data configurate
    about refatored
    projects refatored

---
+++ modulo_03/data.js
@@ -0,0 +1,20 @@
+module.exports = [     //exportar para o servidor
+    {
+        id: "casa_criativa",
+        alt: "Casa Criativa",
+        img_src: "_imgs/logo1.png",
+        description: "Ideias para você realizar em casa durante a quarentena"
+    },
+    {
+        id: "project_img",
+        al:"Google Glass",
+        img_src: "_imgs/glass-logo-peq.jpg",
+        description: "Veja o mundo de uma forma diferente com o novo Google Glass"
+    },
+    {
+        id: "launchbase_img",
+        al:"Launchbase",
+        img_src: "_imgs/launchbase_logo.png",
+        description: ""
+    }
+] 
--- modulo_03/server.js
+++ modulo_03/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const nunjucks = require('nunjucks') 
 
 const server = express() 
+const informations = require("./data")
 
 server.use(express.static('public')) //ainda não sei
 
@@ -9,15 +10,25 @@ server.set("view engine", "njk") //qual tipo de arquivo o servidor está lendo
 
 
 nunjucks.configure("views", { //configuração do nunjucks
-    express: server
+    express: server,
+    noCache: true,
+    autoescape: false
 })
 
-server.get("/", function(req, res) { // rota para about
-    return res.render("about")
+server.get("/", function(req, res) { // rota para about - rota raíz
+    const data = {
+        img: "https://scontent.fbsb8-2.fna.fbcdn.net/v/t1.0-9/s960x960/88166282_2879810075445688_6102658720955629568_o.jpg?_nc_cat=109&_nc_sid=85a577&_nc_eui2=AeFMnyKXI7jAagB5Mt3CBnxo8PPATaDhFGjw88BNoOEUaD51laDz8HmMzmZVw4hu0Bp2UHsfJpHuef9ZNPLGJRQa&_nc_ohc=1l7oQqzIqN4AX9JLgRO&_nc_ht=scontent.fbsb8-2.fna&_nc_tp=7&oh=2cf6b51d0e9f6b595dc3c2ec5a127d1b&oe=5EB1809D",
+        name: "Lucas Felinto",
+        role: "Full Stack Developer",
+        education: "Estudante de Análise e Desenvolvimento de Sistemas",
+        tecnologies: "HTML | CSS | JavaScript | Node.js"
+    }
+
+    return res.render("about" , {about: data})
 })
 
 server.get("/projects", function(req, res) { // rota para projects
-    return res.render("projects")
+    return res.render("projects", {items: informations}) //passando a variável projects para o nunjucjs projects
 }) 
 
 server.listen(5000, function (){
